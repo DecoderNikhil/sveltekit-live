a72f9ffecd82585174eace15c6347017f3e025bb
Nikhil <nikhils9802@gmail.com>
2024-07-15 13:00:29 +0530

    added postgres

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,5 +1,11 @@
-<script>
+<script lang="ts">
     export let data;
 </script>
+
+<div>
+  {#each data.names  as { name}}
+    <div class="font-semibold ">{name}</div>
+  {/each}
+</div>
 
 <p class="text-center font-bold text-orange-500"> {data.content} </p>
